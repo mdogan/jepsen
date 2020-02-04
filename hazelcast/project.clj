8ff9bd161e2753f91119ed91ff3b3d72c496d3b0
Mehmet Dogan <mehmet@dogan.io>
2020-02-04 16:34:47 +0300

    wip

--- hazelcast/project.clj
+++ hazelcast/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [jepsen "0.1.18-SNAPSHOT3"]
-                 [com.hazelcast/hazelcast-enterprise "4.0-SNAPSHOT"]]
+                 [com.hazelcast/hazelcast-enterprise "4.1-SNAPSHOT"]]
   :repositories {"hazelcast snapshot" "https://repository.hazelcast.com/snapshot/"
                  "hazelcast release" "https://repository.hazelcast.com/release/"}
   :aot :all
